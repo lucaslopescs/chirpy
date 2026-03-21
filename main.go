c876938575c336d25b9d70954876beb4502bd7ff
lucaslopescs <lucasglopes22@gmail.com>
2026-03-21 00:09:14 -0400

    finished chapter 1

--- main.go
+++ main.go
@@ -1,24 +1,46 @@
 package main
+
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
-	
+
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))	
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
+	mux.HandleFunc("/healthz", handler)
 
 	srv := &http.Server{
-		Addr: ":" + port,
-  		Handler: mux,
+		Addr:    ":" + port,
+		Handler: mux,
 	}
-	
+
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
-
-	
-
