40ce4e85026216c51f92f32f40ee0aaf39eabdbc
lucaslopescs <lucasglopes22@gmail.com>
2026-03-21 17:20:43 -0400

    finished CH2 & CH3

--- main.go
+++ main.go
@@ -19,9 +19,16 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(
+		`<html>
+  			<body>
+    			<h1>Welcome, Chirpy Admin</h1>
+    			<p>Chirpy has been visited %d times!</p>
+  			</body>
+		</html>`,
+		cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
@@ -43,12 +50,17 @@ func main() {
 
 	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
-		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/app/",
+		apiCfg.middlewareMetricsInc(
+			http.StripPrefix(
+				"/app",
+				http.FileServer(
+					http.Dir(
+						filepathRoot)))))
 
-	mux.HandleFunc("GET /healthz", handler)
-	mux.HandleFunc("GET /metrics", apiCfg.handleMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handleReset)
+	mux.HandleFunc("GET /api/healthz", handler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handleReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
